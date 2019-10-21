2b80dbff6c12366da9aead153f20e45af6192f98
antiwinforever <antiwinforever@gmail.com>
2019-10-21 12:12:20 +0300

    Добавил хранение и вывод истории

--- DZ_Calc_20.10.2019_Backend/Program.cs
+++ DZ_Calc_20.10.2019_Backend/Program.cs
@@ -6,7 +6,10 @@ namespace DZ_Calc_20._10._2019_Backend
     {
         static void Main(string[] args)
         {
+            int One = 0, Two = 0, Key = 0;
             Console.WriteLine("DPV.Soft_Calc_1.0.1");
+            string[] history = new string[5];
+            int i = 0;
             Console.WriteLine("Для продолжения нажмите любую клавишу...");
             Console.ReadKey();
             Console.Clear();
@@ -16,8 +19,9 @@ namespace DZ_Calc_20._10._2019_Backend
                     "Введите 2 для вычитания одного из другого\n" +
                     "Введите 3 для умножения\n" +
                     "Введите 4 для деления\n" +
-                    "Введите 5 для выхода");
-                int Key = Convert.ToInt32(Console.ReadLine());
+                    "Введите 5 для выхода\n" +
+                    "Введите 6 для вывода истории");
+                Key = Convert.ToInt32(Console.ReadLine());
                 Console.Clear();
                 if (Key == 5)
                 {
@@ -25,36 +29,51 @@ namespace DZ_Calc_20._10._2019_Backend
                     Console.ReadKey();
                     break;
                 }
-                Console.Write("Введите первое число: ");
-                int One = Convert.ToInt32(Console.ReadLine());
-                Console.Clear();
-                Console.Write("Введите второе число: ");
-                int Two = Convert.ToInt32(Console.ReadLine());
-                Console.Clear();
-                //Console.WriteLine("Key={0}, One={1}, Two={2}", Key, One, Two);
-                //Console.Read();
+                if (Key != 6)
+                {
+                    Console.Write("Введите первое число: ");
+                    One = Convert.ToInt32(Console.ReadLine());
+                    Console.Clear();
+                    Console.Write("Введите второе число: ");
+                    Two = Convert.ToInt32(Console.ReadLine());
+                    Console.Clear();
+                }
+                
+                
+               
 
-
+                if (i > 4) i = 0;
                 
                 switch (Key)
                 {
                     case 1:
-                        Console.WriteLine("Сложение:\n{1}+{2}={0}", One + Two, One, Two);
+                        history[i] = "Сложение:\n" + One + "+" + Two + "=" + (One + Two);
+                        Console.WriteLine(history[i]);
                         break;
                     case 2:
-                        Console.WriteLine("Вычитание:\n{1}-{2}={0}", One - Two, One, Two);
+                        history[i] = "Вычитание:\n" + One + "-" + Two + "=" + (One - Two);
+                        Console.WriteLine(history[i]);
                         break;
                     case 3:
-                        Console.WriteLine("Умножение:\n{1}*{2}={0}", One * Two, One, Two);
+                        history[i] = "Умножение:\n" + One + "*" + Two + "=" + (One * Two);
+                        Console.WriteLine(history[i]);
                         break;
                     case 4:
-                        Console.WriteLine("Деление:\n{1}/{2}={0}", One / Two, One, Two);
+                        history[i] = "Деление:\n" + One + "/" + Two + "=" + (One / Two);
+                        Console.WriteLine(history[i]);
+                        break;
+                    case 6:
+                        Console.WriteLine("История:");
+                        foreach (var his in history)
+                        {
+                            if (his != null) Console.WriteLine(his);
+                        }
                         break;
                     default:
                         break;
                 }
 
-
+                i++;
 
 
                 Console.WriteLine("\nДля продолжения нажмите любую клавишу...");
